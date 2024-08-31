f0b30cc2af2a544a9ce71eac729bada520cc79e9
Sithu <cithukyaw@gmail.com>
2024-08-31 12:09:49 +0630

    Add Navbar component

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,34 @@
+import {FC} from "react";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import WalletIcon from '@mui/icons-material/Wallet';
+import HomeIcon from "@mui/icons-material/Home";
+import {NavLink, NavLinkRenderProps} from "react-router-dom";
+import "./Navbar.scss";
+
+const Navbar: FC = () => {
+  const menuActive = (navData: NavLinkRenderProps) => navData.isActive ? "active" : "";
+
+  return (
+    <nav>
+      <NavLink to="/dashboard" className={menuActive}>
+        <HomeIcon />
+        <div>Home</div>
+      </NavLink>
+      <NavLink to="/expenses" className={menuActive}>
+        <DashboardIcon />
+        <div>Expense</div>
+      </NavLink>
+      <NavLink to="/ledger" className={menuActive}>
+        <WalletIcon />
+        <div>Saving</div>
+      </NavLink>
+      <NavLink to="/account" className={menuActive}>
+        <AccountCircleIcon />
+        <div>Account</div>
+      </NavLink>
+    </nav>
+  )
+}
+
+export default Navbar;
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,12 +1,16 @@
 import {FC} from "react";
 import {Box} from "@mui/material";
+import Navbar from "../../components/Navbar/Navbar.tsx";
 
 const Dashboard: FC = () => {
   return (
-    <Box sx={{textAlign: "center"}}>
-      <h1>Dashboard</h1>
-      <div>Coming Soon!</div>
-    </Box>
+    <>
+      <Box sx={{textAlign: "center"}}>
+        <h1>Dashboard</h1>
+        <div>Coming Soon!</div>
+      </Box>
+      <Navbar/>
+    </>
   );
 }
 
