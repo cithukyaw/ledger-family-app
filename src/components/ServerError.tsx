62d9c826343a09d38bf9d24dfb326b2c2fcf01d0
Sithu <cithukyaw@gmail.com>
2024-09-10 13:25:15 +0630

    Update ServerError component design

--- src/components/ServerError.tsx
+++ src/components/ServerError.tsx
@@ -1,8 +1,18 @@
 import {FC} from "react";
-import {Box} from "@mui/material";
+import {Box, Container} from "@mui/material";
+import Header from "./Header/Header.tsx";
+import Navbar from "./Navbar/Navbar.tsx";
 
 const ServerError: FC = () => {
-  return <Box className="center-content">Some data could not be load because of server error.</Box>
+  return (
+    <Box className="app">
+      <Header title="Sever Error" />
+      <Container maxWidth="md">
+        <Box className="center-content">Some data could not be load because of server error.</Box>
+      </Container>
+      <Navbar />
+    </Box>
+  )
 }
 
 export default ServerError;
