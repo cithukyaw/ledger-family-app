6146a724524d06edef3591995224edd5d5b3ce86
Sithu <cithukyaw@gmail.com>
2025-05-04 12:15:48 +0630

    Add floating button to add expense

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,34 +1,70 @@
-import {FC} from "react";
+import {FC, useEffect, useState} from "react";
+import {useLocation, NavLink, NavLinkRenderProps} from "react-router-dom";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import WalletIcon from '@mui/icons-material/Wallet';
 import HomeIcon from "@mui/icons-material/Home";
-import {NavLink, NavLinkRenderProps} from "react-router-dom";
+import AddIcon from "@mui/icons-material/Add";
+import Fab from "@mui/material/Fab";
 import "./Navbar.scss";
 
 const Navbar: FC = () => {
+  const [isBottom, setIsBottom] = useState(false);
+  const location = useLocation(); // Get the current route
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.innerHeight + window.scrollY;
+      const pageHeight = document.documentElement.offsetHeight;
+      setIsBottom(scrollPosition >= pageHeight);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   const menuActive = (navData: NavLinkRenderProps) => navData.isActive ? "active" : "";
 
   return (
-    <nav>
-      <NavLink to="/dashboard" className={menuActive}>
-        <HomeIcon />
-        <div>Home</div>
-      </NavLink>
-      <NavLink to="/ledger" className={menuActive}>
-        <WalletIcon />
-        <div>Ledger</div>
-      </NavLink>
-      <NavLink to="/expense" className={menuActive}>
-        <DashboardIcon />
-        <div>Expense</div>
-      </NavLink>
-      <NavLink to="/account" className={menuActive}>
-        <AccountCircleIcon />
-        <div>Account</div>
-      </NavLink>
-    </nav>
-  )
+    <>
+      <nav>
+        <NavLink to="/dashboard" className={menuActive}>
+          <HomeIcon />
+          <div>Home</div>
+        </NavLink>
+        <NavLink to="/ledger" className={menuActive}>
+          <WalletIcon />
+          <div>Ledger</div>
+        </NavLink>
+        <NavLink to="/expense" className={menuActive}>
+          <DashboardIcon />
+          <div>Expense</div>
+        </NavLink>
+        <NavLink to="/account" className={menuActive}>
+          <AccountCircleIcon />
+          <div>Account</div>
+        </NavLink>
+      </nav>
+      {location.pathname !== "/expense/add" && ( // Hide Fab button on /expense/add
+        <Fab
+          className="fab-button"
+          color="primary"
+          aria-label="add"
+          style={{
+            position: "fixed",
+            bottom: "75px",
+            right: "15px",
+            opacity: isBottom ? 0 : 1,
+            transition: "opacity 0.3s ease-in-out"
+          }}
+          component={NavLink}
+          to="/expense/add"
+        >
+          <AddIcon />
+        </Fab>
+      )}
+    </>
+  );
 }
 
 export default Navbar;
